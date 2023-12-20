19c06c33cccb89d6d94a639426c0b496dd8710ec
Kevin <kevin@kokev.de>
2023-12-20 12:03:03 +0100

    Change glusterd repo image

--- src/node.rs
+++ src/node.rs
@@ -150,7 +150,7 @@ impl GlusterdNode {
                         containers: vec![Container {
                             name: "glusterd".to_string(),
                             // TODO: allow external image and tag for renovate
-                            image: Some("ghcr.io/koskev/glusterfs-image:2023.11.26".to_string()),
+                            image: Some("ghcr.io/koskev/glusterd-image:2023.12.20".to_string()),
                             args: Some(vec!["-L".to_string(), "DEBUG".to_string()]),
 
                             volume_mounts: Some(volume_mounts),
