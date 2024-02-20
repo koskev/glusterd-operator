8847110aa424aa7db79b2ea6aeea2b1228ebbc86
Kevin <kevin@kokev.de>
2024-02-20 01:51:59 +0100

    Implement a getname method for metadata to avoid a bunch of unwraps

--- src/utils.rs
+++ src/utils.rs
@@ -1,3 +1,15 @@
+use kube::core::ObjectMeta;
+
 pub fn get_label(id: &str) -> String {
     format!("glusterd-{}", id)
+}
+
+pub trait MetaName {
+    fn get_name(&self) -> String;
+}
+
+impl MetaName for ObjectMeta {
+    fn get_name(&self) -> String {
+        self.name.clone().unwrap_or("unknown-name".to_string())
+    }
 }
